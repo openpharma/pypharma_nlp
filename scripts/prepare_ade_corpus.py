3ae97ea2c0695b4437ef285259032a5c79fe4663
Diego Doe <diegovs87@yahoo.fr>
2019-08-31 16:04:04 +0200

    added ade corpus script

---
+++ scripts/prepare_ade_corpus.py
@@ -0,0 +1,16 @@
+from pypharma_nlp.data.ade_corpus import download_source_data
+from pypharma_nlp.data.ade_corpus import get_classification_examples
+import os
+
+
+# Data directories
+SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+DATA_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "..", "data", "ade_corpus")
+
+print("Downloading the ADE Corpus V2")
+download_source_data(DATA_DIRECTORY)
+
+print("Processing the ADE Corpus V2")
+for sentences, labels in get_classification_examples(DATA_DIRECTORY):
+    for i in range(len(sentences)):
+        print(sentences[i][:50], labels[i])
