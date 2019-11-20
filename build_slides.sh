5190bb87223038c4968522d380c5b657864f694d
Diego Doe <diegovs87@yahoo.fr>
2019-11-21 00:05:01 +0100

    updated slides

--- build_slides.sh
+++ build_slides.sh
@@ -8,9 +8,16 @@ pandoc \
     --bibliography slides/bibliography.bib \
     -o slides.pdf
 
-pandoc \
-    -t beamer slides/summary.md \
-    -V theme:metropolis \
-    --filter pandoc-citeproc \
-    --bibliography slides/bibliography.bib \
-    -o summary.pdf
+#pandoc \
+#    -t beamer slides/summary.md \
+#    -V theme:metropolis \
+#    --filter pandoc-citeproc \
+#    --bibliography slides/bibliography.bib \
+#    -o summary.pdf
+#
+#pandoc \
+#    -t beamer slides/opening.md \
+#    -V theme:metropolis \
+#    --filter pandoc-citeproc \
+#    --bibliography slides/bibliography.bib \
+#    -o opening.pdf
